6d153154d2b776cf04cc8bfedc3311f57c95e39a
Evance262 <evancechapuma62@gmail.com>
2022-06-15 15:00:27 +0200

    node

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,30 +1,16 @@
 module.exports = {
   env: {
-    browser: false,
-    es6: true,
-    jest: true,
+    browser: true,
+    es2021: true,
   },
   extends: [
     'airbnb-base',
-    'plugin:jest/all',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['jest'],
   rules: {
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement',
-    ],
   },
   overrides:[
     {
