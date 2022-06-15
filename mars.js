93ee2be990000fb952b78eb800c9dd40938b2349
Evance262 <evancechapuma62@gmail.com>
2022-06-15 02:28:47 +0200

    Hello backend

---
+++ mars.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+function myFun(a,  b, ...manyMoreArgs) {
+  console.log("a", a)
+  console.log("b", b)
+  console.log("manyMoreArgs", manyMoreArgs)
+}
+
+console.log(myFun("one", "two", "three", "four", "five", "six"))
